154ba66816c485134c9e31d45262ac62a3fe1175
iris <ikohler@calpoly.edu>
2019-01-31 10:05:24 -0800

    Stubbed loop unrolling methods

--- matadd.c
+++ matadd.c
@@ -1,9 +1,43 @@
 
 /*
+ * matrix addition
  * inputs:
  *    A, B, C: pointers to height x width matrices
  *    C <- A + B
  */
-void matadd(int **C, int **A, int **B, int height, int width) {
+static void matadd(int **C, int **A, int **B, int height, int width) {
+
+}
+
+/*
+ * matrix addition
+ * inputs:
+ *    A, B, C: pointers to height x width matrices
+ *    C <- A + B
+ * implements loop unrolling level 2
+ */
+static void matadd2(int **C, int **A, int **B, int height, int width) {
+
+}
+
+/*
+ * matrix addition
+ * inputs:
+ *    A, B, C: pointers to height x width matrices
+ *    C <- A + B
+ * implements loop unrolling level 4
+ */
+static void matadd4(int **C, int **A, int **B, int height, int width) {
+
+}
+
+/*
+ * matrix addition
+ * inputs:
+ *    A, B, C: pointers to height x width matrices
+ *    C <- A + B
+ * implements loop unrolling level 8
+ */
+static void matadd8(int **C, int **A, int **B, int height, int width) {
 
 }
