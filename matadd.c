fcf257de1bd1b3f76693aeb1c987c230bbaf9ac8
iris <ikohler@calpoly.edu>
2019-01-31 10:01:38 -0800

    Added method stub

--- matadd.c
+++ matadd.c
@@ -1,2 +1,9 @@
-/* blah */
-/* hello */
+
+/*
+ * inputs:
+ *    A, B, C: pointers to height x width matrices
+ *    C <- A + B
+ */
+void matadd(int **C, int **A, int **B, int height, int width) {
+
+}
