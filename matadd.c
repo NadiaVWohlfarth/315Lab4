3350b786c4c1b35cfb50d334ce7acec75e662e66
iris <ikohler@calpoly.edu>
2019-02-07 09:23:51 -0800

    Commented out loop unrolled function stubs for now

--- matadd.c
+++ matadd.c
@@ -16,9 +16,9 @@ static void matadd(int **C, int **A, int **B, int height, int width) {
  *    C <- A + B
  * implements loop unrolling level 2
  */
-static void matadd2(int **C, int **A, int **B, int height, int width) {
-   int i, j; /* used to index into matrices */
-}
+/* static void matadd2(int **C, int **A, int **B, int height, int width) {
+   int i, j;
+} */
 
 /*
  * matrix addition
@@ -27,9 +27,9 @@ static void matadd2(int **C, int **A, int **B, int height, int width) {
  *    C <- A + B
  * implements loop unrolling level 4
  */
-static void matadd4(int **C, int **A, int **B, int height, int width) {
-   int i, j; /* used to index into matrices */
-}
+/* static void matadd4(int **C, int **A, int **B, int height, int width) {
+   int i, j;
+} */
 
 /*
  * matrix addition
@@ -38,6 +38,6 @@ static void matadd4(int **C, int **A, int **B, int height, int width) {
  *    C <- A + B
  * implements loop unrolling level 8
  */
-static void matadd8(int **C, int **A, int **B, int height, int width) {
-   int i, j; /* used to index into matrices */
-}
+/* static void matadd8(int **C, int **A, int **B, int height, int width) {
+   int i, j;
+} */
