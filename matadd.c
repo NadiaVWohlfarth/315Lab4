0134e228643d6fc254a977e1e1667d39ea2e8133
iris <ikohler@calpoly.edu>
2019-02-07 10:07:15 -0800

    Created main function

--- matadd.c
+++ matadd.c
@@ -1,3 +1,88 @@
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 9
+
+int main(int argc, char* argv[]) {
+   int[][] A, B, C;
+   int i, j;
+   int* temp;
+
+   /* Allocate A */
+   A = malloc(sizeof(int*) * ROWS);
+   if (NULL == A) {
+      exit(EXIT_FAILURE);
+   }
+
+   /* Fill out A */
+   for (i = 0; i < ROWS; i++) {
+      /* Allocate next row */
+      temp = malloc(sizeof(int) * COLS);
+      if (NULL == temp) {
+         exit(EXIT_FAILURE);
+      }
+      A[i] = temp;
+
+      /* Fill out columns */
+      for (j = 0; j < COLS; j++) {
+         A[i][j] = COLS + j;
+      }
+   }
+
+   /* Allocate B */
+   B = malloc(sizeof(int) * ROWS);
+   if (NULL == B) {
+      exit(EXIT_FAILURE)
+   }
+
+   /* Fill out B */
+   for (i = 0; i < ROWS; i++) {
+      /* Allocate next row */
+      temp = malloc(sizeof(int) * COLS);
+      if (NULL == temp) {
+         exit(EXIT_FAILURE);
+      }
+      B[i] = temp;
+
+      /* Fill out columns */
+      for (j = 0; j < COLS; j++) {
+         B[i][j] = (COLS - j) * ROWS * (-1 * (j % 2));
+      }
+   }
+
+   /* Allocate C */
+   C = malloc(sizeof(int) * ROWS);
+   if (NULL == C) {
+      exit(EXIT_FAILURE)
+   }
+
+   /* Fill out C */
+   for (i = 0; i < ROWS; i++) {
+      /* Allocate next row */
+      temp = calloc(sizeof(int) * COLS);
+      if (NULL == temp) {
+         exit(EXIT_FAILURE);
+      }
+      C[i] = temp;
+   }
+
+   /* Function call */
+   matadd(C, A, B, ROWS, COLS);
+
+   /* Free matrices */
+   for (i = 0; i < ROWS; i++) {
+      free(A[i]);
+      free(B[i]);
+      free(C[i]);
+   }
+   free(A);
+   free(B);
+   free(C);
+
+   return EXIT_SUCCESS;
+}
+
+
+
 
 /*
  * matrix addition
